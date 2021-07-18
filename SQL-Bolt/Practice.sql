f07add2b24ce7ee5736152046f09a349dc761a57
Vlad Aleksandrov <hilk.2013@yandex.ru>
2021-07-18 19:43:20 +0300

    few correct solutions

--- SQL-Bolt/Practice.sql
+++ SQL-Bolt/Practice.sql
@@ -250,3 +250,42 @@ UPDATE movies
 SET title='Toy Story 3',
     director='Lee Unkrich'
 WHERE title='Toy Story 8';
+
+-- SQL Lesson 15: Deleting rows 
+-- This database is getting too big, lets remove all movies that were released before 2005.
+DELETE FROM movies
+WHERE year<2005;
+
+-- Andrew Stanton has also left the studio, so please remove all movies directed by him.
+DELETE FROM movies
+WHERE director='Andrew Stanton';
+
+-- SQL Lesson 16: Creating tables
+/* Create a new table named Database with the following columns:
+– Name A string (text) describing the name of the database
+– Version A number (floating point) of the latest version of this database
+– Download_count An integer count of the number of times this database was downloaded
+This table has no constraints. */
+
+CREATE TABLE Database (
+    name TEXT,
+    version FLOAT,
+    download_count INTEGER
+);
+
+-- SQL Lesson 17: Altering tables 
+-- Add a column named Aspect_ratio with a FLOAT data type to store the aspect-ratio each movie was released in. 
+ALTER TABLE movies
+ADD Aspect_ratio FLOAT;
+
+-- Add another column named Language with a TEXT data type to store the language that the movie was released in. Ensure that the default for this language is English.
+ALTER TABLE movies
+ADD Language TEXT
+DEFAULT English;
+
+-- SQL Lesson 18: Dropping tables
+-- We've sadly reached the end of our lessons, lets clean up by removing the Movies table
+DROP TABLE Movies;
+
+-- And drop the BoxOffice table as well
+DROP TABLE Boxoffice;
